2b5dbe49867414279359dfe2b4c4942b5267431d
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2022-09-08 11:01:19 -0700

    modularizing export_burst_coverage.py

--- export_burst_coverage.py
+++ export_burst_coverage.py
@@ -1,69 +1,117 @@
+'''
+A script to export the burst geogrid from the augmented burst map
+
+'''
 import sqlite3
 import json
 import os
 
-path_home = os.getenv('HOME')
 
-PATH_AUGMENTED_BURST_MAP = (f'{path_home}/Documents/DATA/Sensor/Sentinel-1/'
-                            'S1_burstid_20220530/IW/sqlite/'
-                            'burst_map_IW_000001_375887.OPERA-JPL.20220818_113416.sqlite3')
+def extract_burst_geogrid_data(path_augmented_burst_map: str):
+    '''
+    Extract the burst geogrid data from augmented burst map
 
-PATH_TOP=os.path.dirname(__file__)
-PATH_CSV_OUT = f'{PATH_TOP}/data/CSV_OUT_ALL_BURSTS.csv'
-PATH_JSON_OUT = f'{PATH_TOP}/data/CSV_OUT_ALL_BURSTS.json'
-PATH_SQLITE_OUT = f'{PATH_TOP}/data/CSV_OUT_ALL_BURSTS.sqlite'
+    Parameters:
+    -----------
+    path_augmented_burst_map: str
+        Path to the augmented burt map
 
-conn=sqlite3.connect(PATH_AUGMENTED_BURST_MAP)
-curs=conn.cursor()
-curs.execute('SELECT burst_id_jpl, EPSG, xmin, ymin, xmax, ymax FROM burst_id_map')
-query_out=curs.fetchall()
-num_record=len(query_out)
-curs.close()
-conn.close()
+    Return:
+    records_out: list
+        Burst geogrid information
 
-#export to CSV
-print('Exporting to CSV')
-with open(PATH_CSV_OUT,'w+',encoding='utf-8') as fout:
-    #write CSV header
-    fout.write('burst_id, EPSG, xmim, ymin, xmax, ymax\n')
+    '''
 
-    for i, record in enumerate(query_out):
-        fout.write(f'{record[0]}, {record[1]}, {record[2]}, {record[3]}, {record[4]}, {record[5]}\n' )
+    conn = sqlite3.connect(path_augmented_burst_map)
+    curs = conn.cursor()
+    curs.execute('SELECT burst_id_jpl, EPSG, xmin, ymin, xmax, ymax FROM burst_id_map')
+    records_out = curs.fetchall()
 
-# Export to json
-print('Exporting to JSON')
-dict_export={}
-for i in range(num_record):
-    dict_export[query_out[i][0]]={
-        'EPSG':query_out[i][1],
-        'extent':[query_out[i][2],query_out[i][3],query_out[i][4],query_out[i][5]]
-    }
+    curs.close()
+    conn.close()
 
-with open(PATH_JSON_OUT, 'w+', encoding='utf8') as fout:
-    json.dump(dict_export, fout, indent=2)
+    return records_out
 
 
-# Export to SQLITE
-print('Exporting to SQLITE')
-with sqlite3.connect(PATH_SQLITE_OUT) as conn_out:
-    curs_out=conn_out.cursor()
-    curs_out.execute('''CREATE TABLE IF NOT EXISTS burst (
-        burst_id text PRIMARY KEY,
-        EPSG integer,
-        xmin float,
-        ymin float,
-        xmax float,
-        ymax float);
-        ''')
+def export_to_csv(records_out: list, path_csv_out: str):
+    '''
+    Write out the burst geogrid information as .csv file
 
-    for i_burst,record in enumerate(query_out):
-        print(f'{i_burst:,} / {num_record:,}',end='\r')
-        str_sql_command=f'INSERT INTO burst (burst_id ,EPSG, xmin, ymin, xmax, ymax) '\
-                        f'VALUES("{record[0]}", {record[1]}, '\
-                        f'{record[2]}, {record[3]}, '\
-                        f'{record[4]}, {record[5]})'
+    Parameters:
+        records_out: list
+            List of burst geogrid information retrieved from `extract_burst_geogrid_data()`
+        path_csv_out: str
+            path to the .csv file to write out
 
-        curs_out.execute(str_sql_command)
-    print('\n')
-    curs_out.execute('CREATE INDEX index_burst ON burst (burst_id)')
-    conn_out.commit()
+    '''
+    num_record = len(records_out)
+    with open(path_csv_out,'w+',encoding='utf-8') as fout:
+        # write CSV header
+        fout.write('burst_id, EPSG, xmim, ymin, xmax, ymax\n')
+
+        for i_record, record in enumerate(records_out):
+            print(f'Writing: {i_record+1} / {num_record}', end='\r')
+            fout.write(f'{record[0]}, {record[1]}, {record[2]}, ',
+                    f'{record[3]}, {record[4]}, {record[5]}\n' )
+        print('\n')
+        
+
+def export_to_json(records_out: list, path_json_out: str):
+    '''
+    Write out the burst geogrid information as .json file
+
+    Parameters:
+        records_out: list
+            List of burst geogrid information retrieved from `extract_burst_geogrid_data()`
+        path_json_out: str
+            path to the .json file to write out
+
+    '''
+    num_record = len(records_out)
+
+    # Convert the data into dict
+    dict_export={}
+    for i_record in range(num_record):
+        dict_export[records_out[i_record][0]] = {
+            'EPSG':records_out[i_record][1],
+            'extent':[records_out[i_record][2], records_out[i_record][3],
+                    records_out[i_record][4], records_out[i_record][5]]
+        }
+
+    with open(path_json_out, 'w+', encoding='utf8') as fout:
+        json.dump(dict_export, fout, indent=2)
+
+
+def export_to_sqlite(records_out: list, path_sqlite_out: str):
+    '''
+    Write out the burst geogrid information as sqlite database file
+
+    Parameters:
+        records_out: list
+            List of burst geogrid information retrieved from `extract_burst_geogrid_data()`
+        path_sqlite_out: str
+            path to the .sqlite file to write out
+
+    '''
+
+    num_record = len(records_out)
+
+    # Export to SQLITE
+    print('Exporting to SQLITE')
+    with sqlite3.connect(path_sqlite_out) as conn_out:
+        curs_out = conn_out.cursor()
+        curs_out.execute('CREATE TABLE IF NOT EXISTS burst ('
+                         'burst_id text PRIMARY KEY, EPSG integer, '
+                         'xmin float, ymin float, xmax float, ymax float);')
+
+        for i_record, record in enumerate(records_out):
+            print(f'{i_record:,} / {num_record:,}', end='\r')
+            str_sql_command=f'INSERT INTO burst (burst_id ,EPSG, xmin, ymin, xmax, ymax) '\
+                            f'VALUES("{record[0]}", {record[1]}, '\
+                            f'{record[2]}, {record[3]}, '\
+                            f'{record[4]}, {record[5]})'
+
+            curs_out.execute(str_sql_command)
+        print('\n')
+        curs_out.execute('CREATE INDEX index_burst ON burst (burst_id)')
+        conn_out.commit()
