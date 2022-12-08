719492f846828eaad53a926e27dca7933cd10afc
Seongsu Jeong <sjeong.kr@gmail.com>
2022-12-08 07:55:18 -0800

    CLI implemented

--- build_database.py
+++ build_database.py
@@ -19,34 +19,19 @@ from osgeo import ogr, osr
 
 import build_database_core as bd
 
+def main(args):
+    '''
+    Docstring please
+    '''
 
-if __name__=='__main__':
-    
-    PATH_SRC_ROOT = os.getenv('HOME') + '/Documents/DATA/burst_db'
-    PATH_DST_ROOT = os.path.dirname(__file__)
-
-    STR_TIMESTAMP = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-
-    PATH_DATABASE_SRC = f'{PATH_SRC_ROOT}/burst_map_IW_000001_375887.sqlite3'
-    PATH_DATABASE_DST = f'{PATH_DST_ROOT}/data/burst_map_IW_000001_375887.OPERA-JPL.'\
-                        f'{STR_TIMESTAMP}.sqlite3'
-
-    MARGIN_X = 1000.0
-    MARGIN_Y = 1000.0
-    SNAP_X = 30.0
-    SNAP_Y = 30.0
-
-    # other flag
-    export_db_for_opera = True
-    # Flag to choose to add bursts' center
-    # (approximated from bounding boxes' center) and area of the bounding box
-    verbose_database = False
+    if os.path.exists(args.sqlite_path_out):
+        print('Output SQLITE file exists. Removing.')
+        os.remove(args.sqlite_path_out)
 
     # Make a copy of the original ESA burst map data; Play with the duplicate.
-    shutil.copy(PATH_DATABASE_SRC, PATH_DATABASE_DST)
+    shutil.copy(args.sqlite_path_in, args.sqlite_path_out)
 
-
-    conn = sqlite3.connect(PATH_DATABASE_DST)
+    conn = sqlite3.connect(args.sqlite_path_out)
     cur = conn.cursor()
 
     # Add additional columns to the table
@@ -57,11 +42,7 @@ if __name__=='__main__':
     cur.execute('ALTER TABLE burst_id_map ADD COLUMN ymin integer')
     cur.execute('ALTER TABLE burst_id_map ADD COLUMN ymax integer')
 
-    #cur.execute('ALTER TABLE burst_id_map ADD COLUMN xmin float')
-    #cur.execute('ALTER TABLE burst_id_map ADD COLUMN xmax float')
-    #cur.execute('ALTER TABLE burst_id_map ADD COLUMN ymin float')
-    #cur.execute('ALTER TABLE burst_id_map ADD COLUMN ymax float')
-
+    # Create index onthe table to expedite the search process
     cur.execute('CREATE INDEX index_burst ON burst_id_map '\
                 '(relative_orbit_number, burst_id, subswath_name)')
 
@@ -98,7 +79,7 @@ if __name__=='__main__':
 
 
     # Determine EPSG based on the centroid location of the burst in IW2
-    print('Determining EPSG for each bursts')
+    print('Determining EPSG for the bursts on IW2')
     query_result_iw2 = cur.execute('SELECT * FROM burst_id_map WHERE subswath_name = "IW2"')
     col_id = {}
     for i_col, column_desc in enumerate(query_result_iw2.description):
@@ -159,13 +140,23 @@ if __name__=='__main__':
         transform = osr.CoordinateTransformation(srs_in, srs_out)
 
         geom_burst.Transform(transform)
-        dict_geom_tformed = json.loads(geom_burst.ExportToJson())
-        envelope_geom_tformed = geom_burst.GetEnvelope()
+        #dict_geom_tformed = json.loads(geom_burst.ExportToJson())
+        envelope_geom_tformed = list(geom_burst.GetEnvelope())
 
-        xmin = int(np.round((envelope_geom_tformed[0] - MARGIN_X) / SNAP_X) * SNAP_X)
-        xmax = int(np.round((envelope_geom_tformed[1] + MARGIN_X) / SNAP_X) * SNAP_X)
-        ymin = int(np.round((envelope_geom_tformed[2] - MARGIN_Y) / SNAP_Y) * SNAP_Y)
-        ymax = int(np.round((envelope_geom_tformed[3] + MARGIN_Y) / SNAP_Y) * SNAP_Y)
+        # Apply margin
+        envelope_geom_tformed[0] -= args.mxy[0]
+        envelope_geom_tformed[1] -= args.mxy[1]
+        envelope_geom_tformed[2] += args.mxy[0]
+        envelope_geom_tformed[3] += args.mxy[1]
+
+        xmin, ymin, xmax, ymax = bd.snap_extent(tuple(envelope_geom_tformed),
+                                                args.sxy[0],
+                                                args.sxy[1])
+
+        #xmin = int(np.round((envelope_geom_tformed[0] - MARGIN_X) / SNAP_X) * SNAP_X)
+        #xmax = int(np.round((envelope_geom_tformed[1] + MARGIN_X) / SNAP_X) * SNAP_X)
+        #ymin = int(np.round((envelope_geom_tformed[2] - MARGIN_Y) / SNAP_Y) * SNAP_Y)
+        #ymax = int(np.round((envelope_geom_tformed[3] + MARGIN_Y) / SNAP_Y) * SNAP_Y)
 
         str_sql = f'UPDATE  burst_id_map SET '\
                   f'xmin={xmin}, xmax={xmax}, ymin={ymin}, ymax={ymax} WHERE '\
@@ -174,10 +165,10 @@ if __name__=='__main__':
 
         cur.execute(str_sql)
 
-        # dereference the objects from feature
+        # de-reference the objects from feature
         transform = None
         srs_out = None
-        nparr_coord_tformed = None
+        #nparr_coord_tformed = None
 
         # intermediate commit
         if i_row % 1000 == 0:
@@ -188,4 +179,50 @@ if __name__=='__main__':
     print('\n')
     del query_result_all, list_all
 
+    # Generate the deployable DB
+    if args.deployable:
+        if os.path.exists(args.deployable):
+            print(f'Deployable DB exists: {args.deployable}. Deleting.')
+            os.remove(args.deployable)
+
+        records_burst_data = bd.extract_burst_geogrid_data(args.sqlite_path_out)
+        bd.export_to_sqlite(records_burst_data, args.deployable)
+
     print('\nProcessing completed!')
+
+
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(
+                description=('Generate burst bounding box DB for OPERA SAS '
+                             'from ESA burstmap'),
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('sqlite_path_in',
+                        type=str,
+                        help='Path to the source ESA burstmap.')
+
+    parser.add_argument('sqlite_path_out',
+                        type=str,
+                        help='Path to the output sqlite file')
+
+    parser.add_argument('-mxy',
+                        type=float,
+                        default=[5000, 5000],
+                        nargs=2,
+                        help='x/y margin of the bounding boxes in meters')
+
+    parser.add_argument('-sxy',
+                        type=int,
+                        default=[30, 30],
+                        nargs=2,
+                        help='x/y snapping values in meters for bounding boxes')
+
+    parser.add_argument('-d','--deployable',
+                        type=str,
+                        help='(Option) Path to the deployable database')
+
+    args = parser.parse_args()
+
+    main(args)
