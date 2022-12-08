a9b53c3517efa5836b0ed8ec289e26ca14de330d
Seongsu Jeong <sjeong.kr@gmail.com>
2022-12-08 08:35:35 -0800

    version.py added

---
+++ version.py
@@ -0,0 +1,16 @@
+'''
+release history of `burst_db`
+'''
+
+import collections
+
+
+# release history
+Tag = collections.namedtuple('Tag', 'version date')
+release_history = (
+    Tag('0.1.0', '2022-12-08'),
+)
+
+# latest release version number and date
+release_version = release_history[0].version
+release_date = release_history[0].date
