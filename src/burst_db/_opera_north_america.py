0a80093f66374758857d1b9b81c71f6c4a6bddb8
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-07-24 19:14:52 -0400

    add link to opera shape url

--- src/burst_db/_opera_north_america.py
+++ src/burst_db/_opera_north_america.py
@@ -1,3 +1,8 @@
+"""Module to read the OPERA North America shape.
+
+Data comes from:
+https://github.com/OPERA-Cal-Val/DSWx-Validation-Experiments/blob/7f06ab98cf43135eb63e5a29593235dbebcb19fa/marshak/north_america_boundaries/north_america_opera.geojson
+"""
 from pathlib import Path
 
 import geopandas as gpd
