7b6a2f6df16b75d7ab66da9965c0f3d5d7b75c38
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-08-28 10:16:02 -0700

    `1.0.0` -> `0.2.0`

--- src/burst_db/version.py
+++ src/burst_db/version.py
@@ -8,7 +8,7 @@ import collections
 # release history
 Tag = collections.namedtuple('Tag', ['version', 'date'])
 release_history = (
-    Tag('1.0.0', '2023-08-27'),
+    Tag('0.2.0', '2023-08-28'),
     Tag('0.1.2', '2023-07-24'),
     Tag('0.1.1', '2022-12-14'),
     Tag('0.1.0', '2022-12-08')
