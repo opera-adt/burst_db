bb598b969d6126c922e696c7185a4bdaa2f3b8af
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-10-16 10:30:25 -0700

    refactor fetch into base for different queries

--- src/burst_db/cli.py
+++ src/burst_db/cli.py
@@ -2,10 +2,11 @@ import typer
 
 from .build_frame_db import create
 from .query_frame_db import intersect, lookup
-from .query_historical_bursts import fetch_granules
+from .query_historical_bursts import fetch_bursts, fetch_granules
 
 app = typer.Typer()
 app.command()(create)
 app.command()(intersect)
 app.command()(lookup)
+app.command()(fetch_bursts)
 app.command()(fetch_granules)
