85ee68d10bb87884fd5861f6acbb8a8a68197456
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-11-07 15:38:50 -0500

    switch typer to click for cli

--- src/burst_db/cli.py
+++ src/burst_db/cli.py
@@ -1,12 +1,21 @@
-import typer
+import click
 
 from .build_frame_db import create
 from .query_frame_db import intersect, lookup
 from .query_historical_bursts import fetch_bursts, fetch_granules
 
-app = typer.Typer()
-app.command()(create)
-app.command()(intersect)
-app.command()(lookup)
-app.command()(fetch_bursts)
-app.command()(fetch_granules)
+
+@click.group()
+def cli_app():
+    """Create/interact with OPERA's burst/frame databases."""
+    pass
+
+
+cli_app.add_command(create)
+cli_app.add_command(intersect)
+cli_app.add_command(lookup)
+cli_app.add_command(fetch_bursts)
+cli_app.add_command(fetch_granules)
+
+if __name__ == "__main__":
+    cli_app()
