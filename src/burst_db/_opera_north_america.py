8e901ea3e02f2e22593de0f70e4dba32abcd443a
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-02-15 10:23:52 -0500

    use `_with_sub_zone` geojson

--- src/burst_db/_opera_north_america.py
+++ src/burst_db/_opera_north_america.py
@@ -16,7 +16,9 @@ def get_opera_na_shape() -> GeometryType.MULTIPOLYGON:
     Data source
     https://github.com/OPERA-Cal-Val/DSWx-Validation-Experiments/blob/387acb91d7cce3ec62fac47394c219c74b9dee82/marshak/north_america_boundaries/north_america_opera_with_sub_zone.geojson
     """
-    filename = Path(__file__).parent / "data" / "north_america_opera.geojson.zip"
+    filename = (
+        Path(__file__).parent / "data" / "north_america_opera_with_sub_zone.geojson.zip"
+    )
     na_gpd = gpd.read_file(filename)
     # Combine all geometries in the GeoDataFrame into one MultiPolygon
     return na_gpd.geometry.unary_union
