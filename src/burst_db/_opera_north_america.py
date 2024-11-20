7547005ec349465ffa60a39ee91920cecc177d23
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-11-20 13:01:03 -0500

    add pcm shape

--- src/burst_db/_opera_north_america.py
+++ src/burst_db/_opera_north_america.py
@@ -1,7 +1,7 @@
 """Module to read the OPERA North America shape.
 
 Data comes from:
-https://github.com/OPERA-Cal-Val/DSWx-Validation-Experiments/blob/387acb91d7cce3ec62fac47394c219c74b9dee82/marshak/north_america_boundaries/north_america_opera_with_sub_zone.geojson
+https://raw.githubusercontent.com/nasa/opera-sds-pcm/refs/heads/develop/geo/north_america_opera.geojson
 
 """
 
@@ -15,11 +15,9 @@ def get_opera_na_shape() -> GeometryType.MULTIPOLYGON:
     """Read the OPERA North America geometry as a shapely `multipolygon`.
 
     Data source
-    https://github.com/OPERA-Cal-Val/DSWx-Validation-Experiments/blob/387acb91d7cce3ec62fac47394c219c74b9dee82/marshak/north_america_boundaries/north_america_opera_with_sub_zone.geojson
+    https://raw.githubusercontent.com/nasa/opera-sds-pcm/refs/heads/develop/geo/north_america_opera.geojson
     """
-    filename = (
-        Path(__file__).parent / "data" / "north_america_opera_with_sub_zone.geojson.zip"
-    )
+    filename = Path(__file__).parent / "data" / "north_america_opera.geojson.zip"
     na_gpd = gpd.read_file(filename)
     # Combine all geometries in the GeoDataFrame into one MultiPolygon
     return na_gpd.geometry.unary_union
