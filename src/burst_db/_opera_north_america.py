5fd3e382414aa4e806974bb0042f6fb17996176b
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2025-01-07 18:54:28 -0500

    Update North America polygon for expanded coverage
    update for cole polygon

--- src/burst_db/_opera_north_america.py
+++ src/burst_db/_opera_north_america.py
@@ -1,7 +1,7 @@
 """Module to read the OPERA North America shape.
 
 Data comes from:
-https://raw.githubusercontent.com/nasa/opera-sds-pcm/refs/heads/develop/geo/north_america_opera.geojson
+https://raw.githubusercontent.com/nasa/opera-sds-pcm/refs/heads/develop/geo/opera_NA_expanded.geojson
 
 """
 
@@ -15,7 +15,8 @@ def get_opera_na_shape() -> GeometryType.MULTIPOLYGON:
     """Read the OPERA North America geometry as a shapely `multipolygon`.
 
     Data source
-    https://raw.githubusercontent.com/nasa/opera-sds-pcm/refs/heads/develop/geo/north_america_opera.geojson
+    https://raw.githubusercontent.com/nasa/opera-sds-pcm/refs/heads/develop/geo/opera_NA_expanded.geojson
+
     """
     filename = Path(__file__).parent / "data" / "north_america_opera.geojson.zip"
     na_gpd = gpd.read_file(filename)
